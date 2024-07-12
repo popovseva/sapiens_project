3d100ca97785451a82994296dc57d5df82ef9d4e
Vsevolod Popov <popovvv@student.bmstu.ru>
2024-07-12 19:10:33 +0300

    add create_tables_gp.sql

---
+++ create_tables/create_tables_gp.sql
@@ -0,0 +1,141 @@
+CREATE SCHEMA std; -- creating schema in Greenplum
+
+
+-- creating dimension tables
+CREATE TABLE std.stores (
+	plant bpchar(4),
+	txt text
+)
+DISTRIBUTED REPLICATED;
+
+
+
+CREATE TABLE std.promos (
+	promo_id varchar,
+	promo_name varchar,
+	promo_type int4,
+	material int8,
+	discount int4
+)
+DISTRIBUTED REPLICATED;
+
+
+
+CREATE TABLE std.promo_types (
+	promo_type int4,
+	txt text
+)
+DISTRIBUTED REPLICATED;
+
+
+
+-- creating fact tables
+CREATE TABLE std.traffic (
+	plant bpchar(4),
+	date date,
+	time bpchar(6),
+	frame_id bpchar(10),
+	quantity int4
+)
+WITH (
+	appendonly=true,
+	orientation=column,
+	compresstype=zstd,
+	compresslevel=1
+)
+DISTRIBUTED RANDOMLY
+PARTITION BY range(date)
+(
+	start (date '2020-11-01') inclusive 
+	end (date '2022-01-01') exclusive
+	every (interval '1 month'),
+	default partition def
+);
+
+
+
+CREATE TABLE std.bills_head (
+	billnum int8,
+	plant bpchar(4),
+	calday date
+)
+WITH (
+	appendonly=true,
+	orientation=column,
+	compresstype=zstd,
+	compresslevel=1
+)
+DISTRIBUTED by (billnum)
+PARTITION BY RANGE(calday)
+(
+	START (date '2020-11-01') INCLUSIVE 
+	END (date '2021-03-01') EXCLUSIVE
+	EVERY (interval '1 month'),
+	DEFAULT PARTITION def
+);
+
+
+
+CREATE TABLE std.bills_item (
+	billnum int8,
+	billitem int8,
+	material int8,
+	qty int8,
+	netval numeric(17,2),
+	tax numeric(17,2),
+	rpa_sat numeric(17,2),
+	calday date
+)
+WITH (
+	appendonly=true,
+	orientation=column,
+	compresstype=zstd,
+	compresslevel=1
+)
+DISTRIBUTED by (billnum)
+PARTITION BY RANGE(calday)
+(
+	START (date '2020-11-01') INCLUSIVE 
+	END (date '2021-03-01') EXCLUSIVE
+	EVERY (interval '1 month'),
+	DEFAULT PARTITION def
+);
+
+
+
+CREATE TABLE std.coupons (
+	plant varchar
+	date date,
+	coupon_num varchar,
+	promo_id varchar,
+	material int8,
+	billnum int8
+)
+WITH (
+	appendonly=true,
+	orientation=column,
+	compresstype=zstd,
+	compresslevel=1
+)
+DISTRIBUTED by (billnum)
+PARTITION BY RANGE(date)
+(
+	START (date '2020-11-01') INCLUSIVE 
+	END (date '2021-03-01') EXCLUSIVE
+	EVERY (interval '1 month'),
+	DEFAULT PARTITION def
+);
+
+
+
+-- creating log table
+CREATE TABLE std.logs (
+	log_id int8 PRIMARY KEY,
+	log_timestamp timestamp DEFAULT now() NOT NULL,
+	log_type text NOT NULL,
+	log_msg text NOT NULL,
+	log_location text NULL,
+	is_error bool NULL,
+	log_user text DEFAULT "current_user"() NULL
+)
+DISTRIBUTED BY (log_id);
