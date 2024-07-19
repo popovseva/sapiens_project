7a07f11714010102a359de1e52c273ac9c87a5f1
Vsevolod Popov <popovvv@student.bmstu.ru>
2024-07-19 21:09:56 +0300

    update f_full_load_sql

--- functions/f_full_load.sql
+++ functions/f_full_load.sql
@@ -5,32 +5,35 @@ CREATE OR REPLACE FUNCTION std6_116.f_full_load(p_table_to_name text, p_file_nam
 	VOLATILE
 AS $$
 
-	
+
 
 DECLARE
 	v_location   	text := 'std6_116.f_full_load';
 	v_ext_table  	text;
+	v_schema_name 	text;
+	v_table_to_name text;
 	v_file		    text;
+	v_ip			text;
 	v_sql 		    text;
 	v_gpfdist    	text;
 	v_cnt 	 	    int8;
 BEGIN
-	
-	PERFORM std6_116.f_write_log(p_log_type := 'INFO', p_log_message := 'START std6_116.f_full_load', p_location := v_location);
-
+	v_schema_name := left(p_table_to_name,position('.' IN p_table_to_name)-1);
+	v_table_to_name := right(p_table_to_name,length(p_table_to_name) - POSITION('.' IN p_table_to_name));
 	v_ext_table = std6_116.f_unify_name(p_name := p_table_to_name)||'_ext';
+	v_ip = p_ip;
+
+	--logs
+	PERFORM std6_116.f_write_log(p_log_type := 'INFO', p_log_message := 'START std6_116.f_full_load', p_location := v_location);
 	v_gpfdist = 'GPFDIST://'|| p_ip ||'/'||p_file_name||'.CSV';
 	RAISE NOTICE 'EXTERNAL TABLE NAME IS: %, TABLE LOCATION IS: %.', v_ext_table, v_gpfdist;
-	
-	v_sql = 'TRUNCATE TABLE '||p_table_to_name||'; 
-		DROP EXTERNAL TABLE IF EXISTS '||v_ext_table||';
-		CREATE EXTERNAL TABLE '||v_ext_table|| '(LIKE '||p_table_to_name||') 
-		LOCATION('''||v_gpfdist||'''
-		) ON ALL
-		FORMAT ''CSV'' (DELIMITER '';'' NULL '''' ESCAPE ''"'' QUOTE ''"'' HEADER)
-		ENCODING ''UTF8'''; 
-	
+
+	--truncating target	table
+	v_sql = 'TRUNCATE TABLE '||p_table_to_name||'';
 	EXECUTE v_sql;
+
+	-- creating external table
+	v_ext_table = std6_116.f_create_ext_table3(p_source := v_table_to_name, p_ext_protocol := 'GPFDIST', p_ip := v_ip, p_schema_name := v_schema_name);
 
 	EXECUTE 'INSERT INTO '||p_table_to_name||' SELECT * FROM '||v_ext_table||''; 
 	GET DIAGNOSTICS v_cnt = ROW_COUNT;
